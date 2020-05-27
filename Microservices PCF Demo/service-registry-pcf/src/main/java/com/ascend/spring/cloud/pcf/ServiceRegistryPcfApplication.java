4be6f4d654f553d726f69555f73305f5c60610d5
Siddhardha Venuthurupalli <66024694+SiddhardhaVenuthurupalli@users.noreply.github.com>
2020-05-27 21:33:12 +0530

    Add files via upload

---
+++ Microservices PCF Demo/service-registry-pcf/src/main/java/com/ascend/spring/cloud/pcf/ServiceRegistryPcfApplication.java
@@ -0,0 +1,23 @@
+package com.ascend.spring.cloud.pcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class ServiceRegistryPcfApplication {
+	
+	@GetMapping("/")
+	public String getMessage() {
+		return "Welcome to Ascend Program";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryPcfApplication.class, args);
+	}
+
+}
---
+++ Microservices PCF Demo/service-registry-pcf/src/test/java/com/ascend/spring/cloud/pcf/ServiceRegistryPcfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascend.spring.cloud.pcf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryPcfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
