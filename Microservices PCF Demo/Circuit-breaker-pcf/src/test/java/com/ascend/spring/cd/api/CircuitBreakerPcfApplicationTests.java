a4967999083b7de0f96eed9a2052ca65fa2326a8
Siddhardha Venuthurupalli <66024694+SiddhardhaVenuthurupalli@users.noreply.github.com>
2020-05-27 21:28:34 +0530

    Add files via upload

---
+++ Microservices PCF Demo/Circuit-breaker-pcf/src/test/java/com/ascend/spring/cd/api/CircuitBreakerPcfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascend.spring.cd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerPcfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
